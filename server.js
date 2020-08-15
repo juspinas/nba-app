75ec07c879643ec38e83df055a7bdffeef594117
juspinas <38094067+juspinas@users.noreply.github.com>
2020-08-15 17:09:30 +1200

    First commit with local host connected to index.html

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.static("public"));
+app.get('/', (req, res) => res.sendFile(__dirname + "/client/index.html"))
+
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
